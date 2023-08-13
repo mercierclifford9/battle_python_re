0fe8b58dfd856e10171222d8d59d9780017a461b
mercierclifford9 <140321835+mercierclifford9@users.noreply.github.com>
2023-08-12 22:27:51 -0400

    Add files via upload

---
+++ battle_python_re.py
@@ -0,0 +1,15 @@
+fraz=input("Antre fraz ou an")
+compt=1
+for i in fraz:
+    if(i==" "):
+        compt=compt+1
+voy="aeiouy"
+chenn=fraz.split
+position=1
+for j in range (1,7):
+    for i in range(1,6):
+        if(chenn[0][i]==voy[j]):
+            chennn=chenn.capitalize()
+print(chennn)
+
+
